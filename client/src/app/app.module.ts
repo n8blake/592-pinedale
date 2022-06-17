a316e84852bc65486a7ea50dfdd57cdcb3e33601
Nate <n8blake@mac.com>
2022-06-17 11:22:23 -0400

    Include new components in app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { MenuListComponent } from './common/menu-list/menu-list.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { WineListItemComponent } from './drinks/wine-list-item/wine-list-item.component';
+import { BeerListItemComponent } from './drinks/beer-list-item/beer-list-item.component';
+import { WhiskeyListItemComponent } from './drinks/whiskey-list-item/whiskey-list-item.component';
+import { CocktailsListItemComponent } from './drinks/cocktails-list-item/cocktails-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MenuListComponent,
+    PageNotFoundComponent,
+    WineListItemComponent,
+    BeerListItemComponent,
+    WhiskeyListItemComponent,
+    CocktailsListItemComponent
   ],
   imports: [
     BrowserModule,
