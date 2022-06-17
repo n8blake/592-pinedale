0389b11bd668694bb80f2b2d3882ff8109e01652
Nate <n8blake@mac.com>
2022-06-17 11:21:24 -0400

    Add routes for different menu lists

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuListComponent } from './common/menu-list/menu-list.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'wine', component: MenuListComponent},
+  { path: 'whiskey', component: MenuListComponent},
+  { path: 'beer', component: MenuListComponent},
+  { path: 'cocktails', component: MenuListComponent},
+  { path: '', redirectTo: '/wine', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
