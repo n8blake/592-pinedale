357cdd442fe8ec3d7f474aaccdfee621dfbc42fb
Nate <n8blake@mac.com>
2022-06-19 17:50:29 -0400

    Add drink resolver to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MenuListComponent } from './common/menu-list/menu-list.component';
 import { AddComponent } from './drinks/add/add.component';
+import { DrinkListResolverService } from './drinks/drink-list-resolver.service';
+import { DrinkService } from './drinks/drink.service';
+import { Drinks } from './drinks/drinks.model';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'wine', component: MenuListComponent},
-  { path: 'whiskey', component: MenuListComponent},
-  { path: 'beer', component: MenuListComponent},
-  { path: 'cocktails', component: MenuListComponent},
+  { path: 'wine', component: MenuListComponent, resolve: {
+    drink : DrinkListResolverService
+  }},
+  { path: 'whiskey', component: MenuListComponent, resolve: {
+    drink : DrinkListResolverService
+  }},
+  { path: 'beer', component: MenuListComponent, resolve: {
+    drink : DrinkListResolverService
+  }},
+  { path: 'cocktails', component: MenuListComponent, resolve: {
+    drink : DrinkListResolverService
+  }},
   { path: 'add', component: AddComponent },
-  { path: '', redirectTo: '/wine', pathMatch: 'full'},
+  { path: '', redirectTo: '/cocktails', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
